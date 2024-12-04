f94251672bbce76d3da0116d5ce4796bb88afb5d
Qavot <130914379+Qavot@users.noreply.github.com>
2024-12-04 18:17:11 +0300

    upd

--- my_podiq/my_podiq/settings.py
+++ my_podiq/my_podiq/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8bov!o_m)7ci6*z9&xal&yv#k$^=puw1q_m8+k-w913yzucqre'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
